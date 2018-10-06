36e7f2c7f16d19603272c44743422734577e6b53
francois <francois@frontfundr.com>
2018-10-05 17:19:25 -0700

    proxy

--- Batch/Batch/FlightsEngine/Program.cs
+++ Batch/Batch/FlightsEngine/Program.cs
@@ -20,7 +20,7 @@ namespace FlightsEngine
 
 
 
-        public static  bool SearchFlights(int SearchTripWishesId, string ScrappingFolder, string FirefoxExeFolder)
+        public static bool SearchFlights(int SearchTripWishesId, string ScrappingFolder, string FirefoxExeFolder)
         {
             bool result = false;
             try
@@ -32,9 +32,10 @@ namespace FlightsEngine
 
                 List<ProxyItem> Proxies = ProxyHelper.GetProxies();
 
+                string lastSuccessfullProxy = null;
 
                 SearchTripWishesItem SearchTripWishesItem = _searchTripWishesService.GetSearchTripWishesById(SearchTripWishesId);
-                if (SearchTripWishesItem != null && SearchTripWishesItem._SearchTripWishes!=null)
+                if (SearchTripWishesItem != null && SearchTripWishesItem._SearchTripWishes != null)
                 {
                     var SearchTripWishes = SearchTripWishesItem._SearchTripWishes;
                     result = true;
@@ -60,58 +61,70 @@ namespace FlightsEngine
                             filter.MaxStopsNumber = SearchTripWishes.MaxStopNumber;
 
 
+
                             foreach (var provider in SearchTripWishesItem.ProvidersToSearch)
                             {
                                 if (!provider.HasAPI)
                                 {
                                     if (Proxies != null && Proxies.Count > 0)
                                     {
-                                        string Proxy = ProxyHelper.GetBestProxy(Proxies);
-                                        if (Proxy == null)
+                                        string Proxy = lastSuccessfullProxy;
+                                        if (String.IsNullOrWhiteSpace(Proxy))
                                         {
-                                            Proxies = ProxyHelper.GetProxies();
                                             Proxy = ProxyHelper.GetBestProxy(Proxies);
+                                            if (Proxy == null)
+                                            {
+                                                Proxies = ProxyHelper.GetProxies();
+                                                Proxy = ProxyHelper.GetBestProxy(Proxies);
+                                            }
                                         }
 
 
+                                            ScrappingSearch scrappingSearch = new ScrappingSearch();
+                                            scrappingSearch.Proxy = Proxy;
+                                            scrappingSearch.FirefoxExeFolder = FirefoxExeFolder;
+                                            scrappingSearch.ScrappingFolder = ScrappingFolder;
+                                            int SearchTripProviderId = _searchTripProviderService.InsertSearchTripProvider(provider.Id, searchTrip.Id, Proxy);
+                                            if (SearchTripProviderId > 0)
+                                            {
+                                                scrappingSearch.SearchTripProviderId = SearchTripProviderId;
+                                                scrappingSearch.Provider = provider.Name;
+                                                scrappingSearch.ProxiesList = Proxies;
 
-                                        ScrappingSearch scrappingSearch = new ScrappingSearch();
-                                        scrappingSearch.Proxy = Proxy;
-                                        scrappingSearch.FirefoxExeFolder = FirefoxExeFolder;
-                                        scrappingSearch.ScrappingFolder = ScrappingFolder;
-                                        int SearchTripProviderId = _searchTripProviderService.InsertSearchTripProvider(provider.Id, searchTrip.Id, Proxy);
-                                        if (SearchTripProviderId > 0)
-                                        {
-                                            scrappingSearch.SearchTripProviderId = SearchTripProviderId;
-                                            scrappingSearch.Provider = provider.Name;
-                                            scrappingSearch.ProxiesList = Proxies;
-
-                                            var ScrappingResult = FlighsBot.ScrappingHelper.SearchViaScrapping(filter, scrappingSearch);
-                                            Proxies = ScrappingResult.ProxiesList;
-                                            _searchTripProviderService.SetSearchTripProviderAsEnded(SearchTripProviderId, ScrappingResult.Success, ScrappingResult.LastProxy,ScrappingResult.AttemptsNumber);
-                                            result = result && ScrappingResult.Success;
-                                        }
-                                        else
-                                        {
-                                            result = false;
+                                                var ScrappingResult = FlighsBot.ScrappingHelper.SearchViaScrapping(filter, scrappingSearch);
+                                                Proxies = ScrappingResult.ProxiesList;
+                                                _searchTripProviderService.SetSearchTripProviderAsEnded(SearchTripProviderId, ScrappingResult.Success, ScrappingResult.LastProxy, ScrappingResult.AttemptsNumber);
+                                                result = result && ScrappingResult.Success;
+                                                if (result)
+                                                {
+                                                    lastSuccessfullProxy = ScrappingResult.LastProxy;
+                                                }
+                                                else
+                                                {
+                                                    lastSuccessfullProxy = null;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                result = false;
+                                            }
                                         }
                                     }
                                 }
+
+                                //  Task.Factory.StartNew(() => FlighsBot.PythonHelper.Run(filter, scrappingSearch));
+                                // Console.WriteLine("Pythonresult = "+ Pythonresult.Success+" and Error = "+ (Pythonresult.Error??""));
+
+                                //   FlighsBot.Kayak.SearchFlights(filter);
+                                //   FlighsBot.Kayak.SearchFlights(filter);
+
+                                //   FlightsEngine.FlighsAPI.AirFranceKLM.SearchFlights(filter);
+                                //    FlightsEngine.FlighsAPI.AirHob.SearchFlights(filter);
+                                //   FlightsEngine.FlighsAPI.Kiwi.SearchFlights(filter);
+                                // FlightsEngine.FlighsAPI.RyanAir.SearchFlights(filter);
+                                //  FlightsEngine.FlighsAPI.Transavia.SearchFlights(filter);
+
                             }
-                            
-                            //  Task.Factory.StartNew(() => FlighsBot.PythonHelper.Run(filter, scrappingSearch));
-                            // Console.WriteLine("Pythonresult = "+ Pythonresult.Success+" and Error = "+ (Pythonresult.Error??""));
-
-                            //   FlighsBot.Kayak.SearchFlights(filter);
-                            //   FlighsBot.Kayak.SearchFlights(filter);
-
-                            //   FlightsEngine.FlighsAPI.AirFranceKLM.SearchFlights(filter);
-                            //    FlightsEngine.FlighsAPI.AirHob.SearchFlights(filter);
-                            //   FlightsEngine.FlighsAPI.Kiwi.SearchFlights(filter);
-                            // FlightsEngine.FlighsAPI.RyanAir.SearchFlights(filter);
-                            //  FlightsEngine.FlighsAPI.Transavia.SearchFlights(filter);
-
-                        }
                         catch (Exception e2)
                         {
                             result = false;
@@ -120,10 +133,10 @@ namespace FlightsEngine
                     }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 result = false;
-                FlightsEngine.Utils.Logger.GenerateError(e, System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, "SearchTripWishesId = "+  SearchTripWishesId.ToString()+ " and ScrappingFolder = " + ScrappingFolder + " and FirefoxExeFolder = " + FirefoxExeFolder);
+                FlightsEngine.Utils.Logger.GenerateError(e, System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, "SearchTripWishesId = " + SearchTripWishesId.ToString() + " and ScrappingFolder = " + ScrappingFolder + " and FirefoxExeFolder = " + FirefoxExeFolder);
             }
             return result;
         }
